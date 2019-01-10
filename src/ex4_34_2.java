06827be0630ce955cf1863a249574672ec5d4817
Victor <victor.vanp@gmail.com>
2019-01-10 13:40:46 +0100

    fixed zero hourOfDay case

--- src/ex4_34_2.java
+++ src/ex4_34_2.java
@@ -1,7 +1,7 @@
 public class ex4_34_2 {
 
     public static boolean bark(boolean barking, int hourOfDay) {
-        if ((hourOfDay > 0) && (hourOfDay < 24)) {
+        if ((hourOfDay >= 0) && (hourOfDay < 24)) {
             if (barking) {
                 return (hourOfDay > 22) || (hourOfDay < 8);
             }
