85c56e3e89c4dd88d5e089109d7e1e42a05219bf
Victor <victor.vanp@gmail.com>
2019-01-10 14:11:58 +0100

    added leap year calculator

---
+++ src/ex4_34_3.java
@@ -0,0 +1,15 @@
+public class ex4_34_3 {
+
+    public static boolean isLeapYear(int year) {
+        if ((year > 0) && (year <= 9999)) {
+            if ((((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
---
+++ testsrc/ex4_34_3Test.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ex4_34_3Test {
+
+    @Test
+    void isLeapYearSmallerThan1ReturnFalse() {
+        assertFalse(ex4_34_3.isLeapYear(0));
+    }
+
+    @Test
+    void isLeapYearLargerThan9999ReturnFalse() {
+        assertFalse(ex4_34_3.isLeapYear(10000));
+    }
+
+    @Test
+    void isLeapYear1600ReturnTrue() {
+        assertTrue(ex4_34_3.isLeapYear(1600));
+    }
+
+    @Test
+    void isLeapYear1000ReturnFalse() {
+        assertFalse(ex4_34_3.isLeapYear(1000));
+    }
+}
