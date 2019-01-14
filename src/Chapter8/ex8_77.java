28453a9a75b35d0410414ba3f5731fa2061e4c82
Victor <victor.vanp@gmail.com>
2019-01-14 18:59:11 +0100

    Added arrays exercise

---
+++ src/Chapter8/ex8_77.java
@@ -0,0 +1,41 @@
+package Chapter8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex8_77 {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] input = getIntegers(5);
+        printArray(input);
+
+        int[] sortedInput = sortIntegers(input);
+        System.out.println("Sorted in ascending order:");
+        printArray(sortedInput);
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " numbers");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element at position " + i + " is: " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sorted = Arrays.copyOf(array,array.length);
+        Arrays.sort(sorted);
+        return sorted;
+    }
+}
