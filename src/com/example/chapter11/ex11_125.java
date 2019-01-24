9b8d35d75e6fc920c0294178a5127ffbe1a0319d
Victor <victor.vanp@gmail.com>
2019-01-24 14:57:27 +0100

    Refactored scanner

--- src/com/example/chapter11/ex11_125.java
+++ src/com/example/chapter11/ex11_125.java
@@ -3,20 +3,18 @@ package com.example.chapter11;
 import java.util.Scanner;
 
 public class ex11_125 {
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Input number: ");
-        X x = new X(scanner.nextInt());
+        X x = new X(new Scanner(System.in));
         x.x();
-        scanner.close();
     }
 
     public static class X {
         private int x;
 
-        public X(int x) {
-            this.x = x;
+        public X(Scanner x) {
+            this.x = x.nextInt();
         }
 
         public void x() {
