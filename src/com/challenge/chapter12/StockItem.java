1ef0673d89557df27fce71cbf7ab85cf0fdde9e0
Victor <victor.vanp@gmail.com>
2019-01-31 18:02:08 +0100

    Sorted collections challenge draft

---
+++ src/com/challenge/chapter12/StockItem.java
@@ -0,0 +1,106 @@
+package com.challenge.chapter12;
+
+public class StockItem implements Comparable<StockItem> {
+    private final String name;
+    private double price;
+    private int quantityStock;
+    private int reserved = 0;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public StockItem(String name, double price, int quantityStock) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = quantityStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int quantityInStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0.0) {
+            this.price = price;
+        }
+    }
+
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals");
+        if (obj == this) {
+            return true;
+        }
+
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering StockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : price " + this.price;
+    }
+
+    public void reserveItem(int quantity) {
+        int remainingQuantity = this.quantityStock - quantity - this.reserved;
+        if (remainingQuantity >= 0) {
+            this.reserved = remainingQuantity;
+        }
+    }
+
+    public int reservedAmount() {
+        return reserved;
+    }
+
+    public void checkout() {
+        this.quantityStock = (this.quantityStock - this.reserved);
+    }
+
+    public void unReserve(int quantity) {
+        int newReservation =  this.reserved - quantity;
+        if (newReservation >= 0) {
+            this.reserved = newReservation;
+        }
+    }
+}
